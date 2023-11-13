ee748963c72f5563dd679e2ff273e12c8a1283c7
JADundee <Jadundee@icloud.com>
2023-11-13 09:28:14 -0700

    express middleware

---
+++ script.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.use((req, res, next) => {
+    console.log('<h1>hello world</h1>')
+    next()
+})
+
+app.get('/', (req, res) => {
+   res.send("getting root")
+   })
+
+app.listen(3000)
